b6a047df248cd01b6579ef59156eaf4d6214623d
Davit Mnatsakanyan <Bastianjung8@gmail.com>
2016-11-16 10:40:51 +0400

    finieshed

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ var data = [];
 function goToSendMessage() {
 
     // get stored urls
-    //var d = JSON.parse(getCookie('data'));
 
     data = JSON.parse(getCookie('data'));
 
@@ -125,16 +124,25 @@ function show_extension(){
             '<div class="ext_content">'+
                 '<div class="cont_left">'+
                     '<div class="name_field">'+
-                        '<input class="full_width" type="text" name="name" placeholder="Input name" value="Davit">'+
+                        '<input class="full_width" type="text" name="name" placeholder="Input name" value="  ">'+
                     '</div>'+
-                    '<textarea placeholder="Write message" class="full_width" name="text" cols="20" rows="3">my message</textarea>'+
+                    '<textarea placeholder="Write message" class="full_width" name="text" cols="20" rows="3">Nice to see You in my connections list ! &#13;&#10; &#13;&#10;'+
+                    'I am ready to assist You for attracting more new clients and keep the current ones more effectively , by developing a fully Responsive Website for Your Business. &#13;&#10; &#13;&#10;'+
+
+                    'I believe, if there would have been a Marketing God, his first commandment would sound exactly like this. &#13;&#10; &#13;&#10;'+
+
+                    '" The Websites in 21th century should be so nice and easy to use and creative, that the users entering there will get an orgasm even ! So, that You will change them into real "fanatics" of Your Brand from just "interested users" by "making" them to fell in love with Your Brand in 3 seconds... &#13;&#10; &#13;&#10;'+
+
+                    'Here You can visit our website.   http://1webproject.com/ &#13;&#10; &#13;&#10;'+
+
+                    'So, do You think You are interested in such services ?</textarea>'+
                 '</div>'+
                 '<div class="cont_right">'+
                     '<div class="right_content"> '+
                         '<h2>Start mail sending?</h2>'+
                         '<div class="btns">' +
                             '<button id="start_visits" class="btn_yes">Yes</button>'+
-                            '<button id="ext_close" class="btn_no">No</button>'+
+                            '<button id="clear" class="btn_no">Clear Cookie</button>'+
                         '</div>'+
                     '</div>'+
                 '</div>'+
@@ -149,30 +157,30 @@ function show_extension(){
     }
 
     //close extension
-    $(document).on('click', '#ext_close', function(){
-        $('.extension').remove();
-    });
+    // $(document).on('click', '#ext_close', function(){
+    //     $('.extension').remove();
+    // });
 
-    //$(document).on('click', '#clear', function(){
-    //    clearCookie();
-    //});
+    $(document).on('click', '#clear', function(){
+       clearCookie();
+    });
 
     // start auto sending
     $(document).on('click', '#start_visits', function(){
         name = $('.ext_content .cont_left input[name="name"]').val();
         text = $('.ext_content .cont_left textarea[name="text"]').val();
 
-        clearCookie();
+       // clearCookie();
 
         if(getCookie('ajaxIndex')){
-            var  ajaxIndex = getCookie('ajaxIndex')
+              ajaxIndex = getCookie('ajaxIndex')
         }
         else{
-            var ajaxIndex = 0;
+             ajaxIndex = 0;
         }
 
         setCookie('name', name, 3600);
-        setCookie('text', text, 3600);
+        setCookie('text', encodeURIComponent(text), 3600);
 
         create_visitable_urls();
         add_cancel_btn();
@@ -185,7 +193,10 @@ function show_extension(){
             }
         }
 
-        goToSendMessage();
+        setTimeout(function(){
+            goToSendMessage();
+        }, 1000);
+
     });
 }
 
@@ -198,7 +209,7 @@ function is_started(){
 function sendMessage() {
 
     if($("#inbox-body input[name='to']").val(getCookie('name'))){
-        if($("#inbox-body textarea[name='message']").text(getCookie('text'))){
+        if($("#inbox-body textarea[name='message']").text(decodeURIComponent(getCookie('text')))){
             if($("#inbox-body textarea[name='message']").click()){
                 if($("#inbox-body input[name='to']").click()){
                     if($("#inbox-body input[name='to']").val(getCookie('name'))){
@@ -209,7 +220,7 @@ function sendMessage() {
                                         ajaxIndex++;
                                         setCookie('ajaxIndex', ajaxIndex, 3600);
                                         goToSendMessage();
-                                    }, 4000)
+                                    }, 5000)
                                 }
                             }
                         }
@@ -219,7 +230,7 @@ function sendMessage() {
                                     ajaxIndex++;
                                     setCookie('ajaxIndex', ajaxIndex, 3600);
                                     goToSendMessage();
-                                }, 4000)
+                                }, 5000)
                             }
                         }
                     }
@@ -268,14 +279,6 @@ $(document).ready(function(){
     var url_arr_1 = url.split('/');
     var url_arr_2 = url.split('&');
 
-    if($('.pagination .next').length > 0){
-        setCookie('next_page', 'yes', 3600);
-    }
-    else{
-        setCookie('next_page', 'no', 3600);
-    }
-
-
     // show extension in first page only
     if(url_arr_1.indexOf('messaging') == -1){
         show_extension();
@@ -283,6 +286,13 @@ $(document).ready(function(){
 
     if(url_arr_1.indexOf('vsearch') == 3){
         if(url_arr_1[4].split('?')[0] == 'p'){
+
+            if($('.pagination .next').length > 0){
+                setCookie('next_page', 'yes', 3600);
+            }
+            else{
+                setCookie('next_page', 'no', 3600);
+            }
 
             // get base url, Extension is OFF
             if(url_arr_2.indexOf('start') == -1)
